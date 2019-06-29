fb1b4065700133eb64d0c90ca7a10a4ea465deae
ruchitha <52199391+kpruchi@users.noreply.github.com>
2019-06-29 09:10:15 +0530

    Add files via upload

---
+++ python Day1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Take x=10 and y=20
+# Print addition and multiplication
+
+# In[3]:
+
+
+x=10 
+y=20
+print(x+y)
+print(x/y)
+print(x%y)
+print(x//y)
+print(x*y)
+print(x-y)
+
+
+# In[4]:
+
+
+x=2
+res=x**5
+print(res)
+
+
+# In[6]:
+
+
+x=1+2**3/4*5
+print(x)
+
+
+# In[ ]:
+
+
+
+
